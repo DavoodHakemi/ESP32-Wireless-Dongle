68bbc2e58c07f6c5956436d541dcae7dffa84509
Davood Hakemi <60563511+DavoodHakemi@users.noreply.github.com>
2026-09-17 23:41:36 +0330

    Run BLE discovery off application core

--- src/services/bluetooth/BluetoothLE.cpp
+++ src/services/bluetooth/BluetoothLE.cpp
@@ -32,7 +32,7 @@ Result<void> BluetoothLE::scan(uint16_t seconds) {
     _done=false;
     _instance=this;
     _scanning=true;
-    if (xTaskCreatePinnedToCore(worker,"bt_ble_scan", 6144, nullptr, 1, nullptr, config::A2DP_TASK_CORE)!=pdPASS) {
+    if (xTaskCreatePinnedToCore(worker,"bt_ble_scan", 6144, nullptr, 1, nullptr, config::BT_SCAN_TASK_CORE)!=pdPASS) {
         _scanning=false;
         return Result<void>::fail(ErrorCode::HardwareError);
     }
